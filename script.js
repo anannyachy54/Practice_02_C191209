848d5ad06097153ba686b20125e213c003e200ad
anannyachy54 <105448900+anannyachy54@users.noreply.github.com>
2023-12-21 00:36:42 +0600

    Practice 02 by C191209

---
+++ script.js
@@ -0,0 +1,70 @@
+/*
+var n1=2;
+var n2=5;
+//var res=n1+n2;
+//var res= n1**n2
+//n1+=10;
+//document.write(n1==n2);
+//n3="2";
+//document.write(n1===n3);
+//document.write(n1!==n2);
+//document.write(n1<=n2);
+//document.write(n1==n2);
+*/
+
+
+
+var num=Number(prompt("Enter a number"));
+if(num%2==0){
+    document.write(num+" is an even number");
+  
+}
+else if(num==0){
+    document.write(num+"  is not odd or even  number");
+  
+}
+else if(num<0){
+    document.write(num+" is a negative number");
+  
+}
+else{
+    document.write(num+"  is an odd number");
+}
+
+/*
+var name=prompt("Enter name");
+var pass=prompt("Enter password");
+if(name=="Ana"){
+    if(pass=="123aa"){
+        document.write("You are logged in");
+    }else{
+            document.write("Wrong Password");
+        }
+    
+
+}else{
+    document.write("Wrong Username");
+}
+*/
+/*
+var n=prompt("enter your fav color");
+switch(n){
+    case "red":
+        document.write("red is my fav color");
+        break;
+        case "black":
+        document.write("black is my fav color");
+        break;
+        case "blue":
+        document.write("black is my fav color");
+        break;
+        default:
+            document.write("no color found");
+}
+*/
+/*
+for(var n=1;n<=100;n++){
+    //document.write("this is " +n+"<br>");
+    document.write(" "+n);
+}
+*/
